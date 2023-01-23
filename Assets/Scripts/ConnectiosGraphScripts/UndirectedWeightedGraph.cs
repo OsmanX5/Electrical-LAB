6a50c0982875c8a579a4cb56ad263a65dd049d3e
Osman yousif <gamemakerx5@gmail.com>
2023-01-23 13:25:42 +0200

    Update UndirectedWeightedGraph.cs

--- Assets/Scripts/ConnectiosGraphScripts/UndirectedWeightedGraph.cs
+++ Assets/Scripts/ConnectiosGraphScripts/UndirectedWeightedGraph.cs
@@ -4,25 +4,34 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+public class Edge
+{
+    public int NextPoint;
+    public float Weight;
+    public Edge(int next, float weight)
+    {
+        NextPoint = next;
+        Weight = weight;
+    }
+}
 public class UndirectedWeightedGraph 
 {
     protected int n = 0;
-    public Dictionary<int,List<Tuple<int, float>>> AdjacencyList = new Dictionary<int, List<Tuple<int, float>>>();
+    protected Dictionary<int, List<Edge>> AdjacencyList = new();
 
-    public bool IsInGraph(int a)=> AdjacencyList.ContainsKey(a);
+    public List<Edge> GetPointConnections(int a)=> AdjacencyList[a];
 
-    public List<Tuple<int,float>> GetPointConnections(int a)=>AdjacencyList[a];
-
-    public bool IsInList(List<Tuple<int, float>> list, Tuple<int, float> a)
+    public bool IsInList(List<Edge> list, Edge a)
     {
-        foreach (var point in list)
+        foreach (var edge in list)
         {
-            if (point.Item1 == a.Item1) return true;
+            if (edge.NextPoint == a.NextPoint) return true;
         }
         return false;
     }
+    public bool IsInGraph(int a) => AdjacencyList.ContainsKey(a);
 
-    
 
-    
+
+
 }
