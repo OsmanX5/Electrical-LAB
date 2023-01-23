8d92532ccb83dab187c6c342b84aacc756247d47
Osman yousif <gamemakerx5@gmail.com>
2023-01-23 13:50:40 +0200

    Update ElectricalGraph.cs

--- Assets/Scripts/ConnectiosGraphScripts/ElectricalGraph.cs
+++ Assets/Scripts/ConnectiosGraphScripts/ElectricalGraph.cs
@@ -4,26 +4,23 @@ using UnityEngine;
 
 public class ElectricalGraph : UndirectedWeightedGraphPathProvider
 {
-    public DisJointSet DisJointSet;
-    public ElectricalGraph() : base()
-    {
-        DisJointSet = new DisJointSet(0);
-    }
     public bool IsInGraph(Point point) => IsInGraph(point.ID);
     public void AddNewPoint(Point point)
     {
-        addPoint(point.ID);
-        DisJointSet.AddPoint(point.ID);
+        AddNewPoint(point.ID);
     }
     public void AddConnection(Point a,Point b,float res)
     {
         AddConnection(a.ID, b.ID, res);
-        DisJointSet.Union(a.ID, b.ID);
     }
     public void RemovePoint(Point a)
     {
-        Debug.Log("Removing " + a.ID);
+        Debug.Log("Removing Point");
         RemovePoint(a.ID);
+    }
+    public void ClearPoint(Point a)
+    {
+        ClearPoint(a.ID);
     }
 
 
